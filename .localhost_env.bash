839f67528893c88cf175a50ec0bdf1530f165ac0
megandamon <megandamon@gmail.com>
2013-10-08 14:07:38 -0400

    Forgot a file

---
+++ .localhost_env.bash
@@ -0,0 +1,12 @@
+#!/usr/local/bin/bash
+
+export CP=/bin/cp
+export TOUCH=/usr/bin/touch
+export QSUB=/bin/csh
+export QSTAT=/usr/pbs/bin/qstat
+export HOSTNAME=/bin/hostname
+export MKDIR=/bin/mkdir
+export LS=/bin/ls
+export CVS=/usr/bin/cvs
+export QDEL=/usr/pbs/bin/qdel
+
